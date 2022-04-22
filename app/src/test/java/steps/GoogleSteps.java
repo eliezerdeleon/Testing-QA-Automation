98b7560471d7b43a0c4ff64a7286ef6c638f6959
eliezerdeleon <djnb.nickyblast@gmail.com>
2022-04-22 15:28:07 -0400

    first commit

---
+++ app/src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,30 @@
+package steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class GoogleSteps {
+    
+@Given("^navego a google$")
+public void navigateToGoogle(){
+
+}
+
+@When("^busco algo$")
+public void searchSomething(){
+
+}
+
+@And("^click on the search button$")
+public void clickOnSearch(){
+
+}
+
+@Then("^obtengo resultados$")
+public void obtainResults(){
+
+}
+
+}
---
+++ app/src/test/resources/Google.feature
@@ -0,0 +1,7 @@
+Feature: Probar la busqueda en google
+
+Scenario: Busco algo en google
+    Given navego a google
+    When busco algo
+    And click on the search button
+    Then obtengo resultados
