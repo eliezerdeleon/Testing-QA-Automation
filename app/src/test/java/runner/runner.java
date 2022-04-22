bbb49ac9834ec442ffcc79cbd82b38385d674d9e
eliezerdeleon <djnb.nickyblast@gmail.com>
2022-04-22 19:23:24 -0400

    Pruebas pasadas

--- app/src/test/java/runner/runner.java
+++ app/src/test/java/runner/runner.java
@@ -9,7 +9,10 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/resources/features/",
-    glue = "src/test/java/steps"
+    glue = "steps"
+
+
+    //glue = "src/test/java/steps"
 
 )
 
