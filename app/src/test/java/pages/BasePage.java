8b4259c38d951456891d00e27b1a5609ca690c12
eliezerdeleon <djnb.nickyblast@gmail.com>
2022-04-23 11:10:07 -0400

    .

--- app/src/test/java/pages/BasePage.java
+++ app/src/test/java/pages/BasePage.java
@@ -1,10 +1,13 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
     
     protected static WebDriver driver;
+    private static WebDriverWait wait;
+    
 
 
 
