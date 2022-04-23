de3f9d351314ec1c3ee07a33e1d1f8a325cf67b2
eliezerdeleon <djnb.nickyblast@gmail.com>
2022-04-22 20:27:00 -0400

    cambios con chromedriver

--- app/src/test/java/steps/GoogleSteps.java
+++ app/src/test/java/steps/GoogleSteps.java
@@ -4,11 +4,15 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import pages.GooglePage;
 
 public class GoogleSteps {
+
+GooglePage google = new GooglePage();
     
 @Given("^I am on the Google search page$")
 public void navigateToGoogle(){
+    google.navigateToGoogle();
 
 }
 
